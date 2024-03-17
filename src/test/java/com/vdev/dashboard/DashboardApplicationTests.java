550049d6e2a727527768bda0af356fa86cee50c2
vinhngo1907 <vinhtrungngo1907@gmail.com>
2024-03-17 15:04:01 +0700

    init source

---
+++ src/test/java/com/vdev/dashboard/DashboardApplicationTests.java
@@ -0,0 +1,11 @@
+package com.vdev.dashboard;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DashboardApplicationTests {
+    @Test
+    void contextLoads(){
+        
+    }
+}
